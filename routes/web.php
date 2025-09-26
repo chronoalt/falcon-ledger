75c4d09f86e8fdbd8e7244373070acdf52cb36c3
chronopad <vincentferdinand.k@gmail.com>
2025-09-26 13:49:33 +0700

    dev: Removed register functionality

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,7 @@ use App\Http\Controllers\AuthController;
 
 # Authentication routes
 Route::middleware(["guest"])->controller(AuthController::class)->group(function() {
-    Route::get("/register", "showRegister")->name("show.register");
     Route::get("/login", "showLogin")->name("show.login");
-    Route::post("/register", "register")->name("register");
     Route::post("/login", "login")->name("login");
 });
 Route::post("/logout", [AuthController::class, "logout"])->name("logout");
