b0546f0897c78eb8fcef4d40e6137b6791c09293
chronopad <vincentferdinand.k@gmail.com>
2025-09-26 14:27:24 +0700

    dev: Changed initial migration, user model, database seeder and factory to accomodate new schema

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,21 +3,20 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Administrator',
+            'email' => 'admin@example.com',
+            'password' => Hash::make("admin123")
         ]);
+
+        // Temporary: Creating 5 users for testing purposes
+        User::factory(5)->create();
     }
 }
