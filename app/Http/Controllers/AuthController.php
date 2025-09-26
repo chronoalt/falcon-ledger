6af2266a0f3ff5e10910e707343ca29b81fe0f33
chronopad <vincentferdinand.k@gmail.com>
2025-09-26 09:47:46 +0700

    dev: Added register functionality and routing

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -13,4 +15,20 @@ class AuthController extends Controller
     public function showLogin() {
         return view("auth.login");
     }
+
+    public function register(Request $request) {
+        $validated = $request->validate([
+            "email" => "required|email|unique:users",
+            "password" => "required|string|min:8|confirmed"
+        ]);
+
+        $user = User::create($validated);
+        Auth::login($user);
+
+        return redirect()->route("show.dashboard");
+    }
+
+    // public function login(): {
+    //     /
+    // }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,17 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name("show.home");
 
 # Authentication routes
 Route::get("/register", [AuthController::class, "showRegister"])->name("show.register");
 Route::get("/login", [AuthController::class, "showLogin"])->name("show.login");
+
+Route::post("/register", [AuthController::class, "register"])->name("register");
+Route::post("/login", [AuthController::class, "login"])->name("login");
+
+# Temporary
+Route::get("/dashboard", action: function() {
+    return view("dashboard");
+})->name("show.dashboard");
