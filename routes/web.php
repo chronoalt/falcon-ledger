7bc433a378fcfc6fd29a5080bc16329699381e70
chronopad <vincentferdinand.k@gmail.com>
2025-09-26 11:17:02 +0700

    dev: Added guest middleware to prevent authenticated user to access authentication routes

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
 # Authentication routes
-Route::get("/register", [AuthController::class, "showRegister"])->name("show.register");
-Route::get("/login", [AuthController::class, "showLogin"])->name("show.login");
-
-Route::post("/register", [AuthController::class, "register"])->name("register");
-Route::post("/login", [AuthController::class, "login"])->name("login");
-
+Route::middleware(["guest"])->controller(AuthController::class)->group(function() {
+    Route::get("/register", "showRegister")->name("show.register");
+    Route::get("/login", "showLogin")->name("show.login");
+    Route::post("/register", "register")->name("register");
+    Route::post("/login", "login")->name("login");
+});
 Route::post("/logout", [AuthController::class, "logout"])->name("logout");
 
 # Authenticated routes
